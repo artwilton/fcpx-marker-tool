518406d5d47678129b1e4871f5c807ade284d29a
artwilton <Arthur.M.Wilton@gmail.com>
2021-06-07 15:57:57 -0400

    added functions to get timeline info and grab clip information from all clips with chapter markers

--- fcpx_marker_tool_v1.py
+++ fcpx_marker_tool_v1.py
@@ -9,16 +9,42 @@ def parse_xml(xml_file):
     root = tree.getroot()
     return root
 
-def grab_chapter_markers(xml_root):
-    chapter_markers = xml_root.findall(".//chapter-marker/..")
-    # print(chapter_markers)
-    for clip in chapter_markers:
-        print(clip.tag, clip.attrib, clip.find("chapter-marker").attrib)
+def get_timeline_info(xml_root):
+    
+    sequence = xml_root.find('.//sequence')
+    format = xml_root.find('./resources/format')
+
+    timeline_info = {
+        'start_frame': sequence.get('tcStart'),
+        'timecode_format': sequence.get('tcFormat'),
+        'frame_rate': format.get('frameDuration')
+    }
+
+    return timeline_info
+
+def grab_clips(xml_root):
+    clips_list = []
+    clips_with_markers = xml_root.findall(".//chapter-marker/..")
+
+    for clip in clips_with_markers:
+        clips_list.append(
+            {
+            'clip_offset': clip.get('offset'),
+            'clip_start': clip.get('start'),
+            'chapter_start': clip.find('chapter-marker').get('start'),
+            'chapter_name': clip.find('chapter-marker').get('value')
+            }
+        )
+
+    return clips_list
 
 def main():
     xml_file = input('Enter xml file path: ')
     parsed_xml = parse_xml(xml_file)
-    grab_chapter_markers(parsed_xml)
+    timeline_info = get_timeline_info(parsed_xml)
+    clips_list = grab_clips(parsed_xml)
+
+    print(timeline_info)
 
 if __name__ == "__main__":
     main()
