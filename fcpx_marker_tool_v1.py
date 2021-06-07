4699b275099dc73c5f017eb416df9e7787ce50b9
artwilton <Arthur.M.Wilton@gmail.com>
2021-06-06 20:17:11 -0400

    setup basic main function

--- fcpx_marker_tool_v1.py
+++ fcpx_marker_tool_v1.py
@@ -1,3 +1,9 @@
 """
 This script takes FCPX .xml files as an input and exports a .txt file containing chapter marker info
 """
+
+def main():
+    print("Run main")
+
+if __name__ == "__main__":
+    main()
