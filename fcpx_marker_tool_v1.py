41ba226a2be0ac7f47ebeb4b7fe53fe3f86da914
artwilton <Arthur.M.Wilton@gmail.com>
2021-06-07 02:19:20 -0400

    testing how to parse clips with chapter markers

--- fcpx_marker_tool_v1.py
+++ fcpx_marker_tool_v1.py
@@ -7,11 +7,18 @@ import xml.etree.ElementTree as ET
 def parse_xml(xml_file):
     tree = ET.parse(xml_file)
     root = tree.getroot()
-    print(root)
+    return root
+
+def grab_chapter_markers(xml_root):
+    chapter_markers = xml_root.findall(".//chapter-marker/..")
+    # print(chapter_markers)
+    for clip in chapter_markers:
+        print(clip.tag, clip.attrib, clip.find("chapter-marker").attrib)
 
 def main():
     xml_file = input('Enter xml file path: ')
-    parse_xml(xml_file)
+    parsed_xml = parse_xml(xml_file)
+    grab_chapter_markers(parsed_xml)
 
 if __name__ == "__main__":
     main()
