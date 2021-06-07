22e3c66d9313ea0b0a95461da734e377c006b313
artwilton <Arthur.M.Wilton@gmail.com>
2021-06-06 22:10:11 -0400

    imported ElementTree module

--- fcpx_marker_tool_v1.py
+++ fcpx_marker_tool_v1.py
@@ -2,8 +2,16 @@
 This script takes FCPX .xml files as an input and exports a .txt file containing chapter marker info
 """
 
+import xml.etree.ElementTree as ET
+
+def parse_xml(xml_file):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+    print(root)
+
 def main():
-    print("Run main")
+    xml_file = input('Enter xml file path: ')
+    parse_xml(xml_file)
 
 if __name__ == "__main__":
     main()
