6c9a7be6af3595a912cb0baca751fe27f68cb5bf
Arthur <arthur@Arthurs-iMac.local>
2021-10-14 00:38:40 -0400

    bug fix in generate_output() function to ensure iteration through multiple clips, also restructured the functionality for checking for NDF footage, and grab_clips() now returns a dictionary with tuples as keys.

--- fcpx_marker_tool_v1.py
+++ fcpx_marker_tool_v1.py
@@ -33,7 +33,7 @@ def split_and_remove_s(rational_time):
 
     return rational_time
 
-def format_frame_rate(frame_rate):
+def frame_rate_to_tuple(frame_rate):
     # Preps framerate for timecode module, ex: the string "1001/30000s" becomes a tuple (30000, 1001)
     frame_rate = split_and_remove_s(frame_rate)
     frame_rate = (frame_rate[1], frame_rate[0])
@@ -41,20 +41,18 @@ def format_frame_rate(frame_rate):
     return frame_rate
 
 def grab_clips(xml_root):
-    clips_list = []
+    clips_dict = {}
     clips_with_markers = xml_root.findall(".//chapter-marker/..")
 
     for clip in clips_with_markers:
-        clips_list.append(
-            {
-            "clip_offset": clip.get("offset"),
-            "clip_start": clip.get("start"),
-            "chapter_start": clip.find("chapter-marker").get("start"),
-            "chapter_name": clip.find("chapter-marker").get("value")
-            }
-        )
 
-    return clips_list
+        clip_offset = clip.get("offset")
+        clip_start = clip.get("start")
+        marker_list = clip.findall("chapter-marker")
+
+        clips_dict[clip_offset, clip_start] = marker_list
+
+    return clips_dict
 
 def get_number_of_frames(rational_time_value, frame_rate):
 
@@ -62,19 +60,18 @@ def get_number_of_frames(rational_time_value, frame_rate):
         return 0
 
     rational_time_tuple = split_and_remove_s(rational_time_value)
-
     number_of_frames = (rational_time_tuple[0] * frame_rate[0]) / (rational_time_tuple[1] * frame_rate[1])
 
     return number_of_frames
 
-def grab_marker_start_time(clip, timeline_info, frame_rate):
+def grab_marker_start_time(clip_offset, clip_start, marker_start, timeline_info, frame_rate):
 
     timeline_starting_frame = get_number_of_frames(timeline_info["start_frame"], frame_rate)
-    clip_offset = get_number_of_frames(clip["clip_offset"], frame_rate)
-    chapter_start = get_number_of_frames(clip['chapter_start'], frame_rate)
-    clip_start = get_number_of_frames(clip['clip_start'], frame_rate)
+    clip_offset_frames = get_number_of_frames(clip_offset, frame_rate)
+    chapter_start_frames = get_number_of_frames(marker_start, frame_rate)
+    clip_start_frames = get_number_of_frames(clip_start, frame_rate)
 
-    chapter_marker_timeline_frame = int((chapter_start - clip_start) + clip_offset + timeline_starting_frame)
+    chapter_marker_timeline_frame = int((chapter_start_frames - clip_start_frames) + clip_offset_frames + timeline_starting_frame)
 
     return chapter_marker_timeline_frame
 
@@ -86,37 +83,50 @@ def frames_to_timecode(frame_count, frame_rate):
 
     return timecode_value
 
-def check_for_NDF(timeline_info):
+def check_for_ndf(timeline_info, frame_rate_tuple):
     # sometimes the framerate will be stored as something like "3003/90000s" instead of "1001/30000s"
     # so this function checks to see if there is any possibility of a match for 29.97 or 59.94 no matter how the framerate is stored
 
-    frame_rate = format_frame_rate(timeline_info["frame_rate"])
     timecode_format = timeline_info["timecode_format"]
+    formatted_frame_rate = frame_rate_tuple
 
-    check_5994 = (frame_rate[0] % 60000 + frame_rate[1] % 1001)
-    check_2997 = (frame_rate[0] % 30000 + frame_rate[1] % 1001)
+    check_5994 = (formatted_frame_rate[0] % 60000 + formatted_frame_rate[1] % 1001)
+    check_2997 = (formatted_frame_rate[0] % 30000 + formatted_frame_rate[1] % 1001)
 
     if (check_5994 == 0) and (timecode_format == "NDF"):
-        frame_rate = (60, 1)
+        formatted_frame_rate = (60, 1)
     elif (check_2997 == 0) and (timecode_format == "NDF"):
-        frame_rate = (3000, 100)
+        formatted_frame_rate = (3000, 100)
 
-    return frame_rate
+    return formatted_frame_rate
 
-def generate_output(clips_list, timeline_info):
+def generate_output(clips_dict, timeline_info):
 
-    for clip in clips_list:
-        frame_rate = check_for_NDF(timeline_info)
-        marker_frame = grab_marker_start_time(clip, timeline_info, frame_rate)
-        marker_timecode = frames_to_timecode(marker_frame, frame_rate)
-        print(marker_timecode)
+    timeline_marker_list = []
+    marker_timecode = ""
+    original_frame_rate = frame_rate_to_tuple(timeline_info["frame_rate"])
+    formatted_frame_rate = check_for_ndf(timeline_info, original_frame_rate)
+
+    for clip_offset, clip_start in clips_dict:
+        clip_marker_list = clips_dict[clip_offset, clip_start]
+
+        for marker in clip_marker_list:
+            marker_start = marker.get("start")
+            marker_frame = grab_marker_start_time(clip_offset, clip_start, marker_start, timeline_info, original_frame_rate)
+            marker_timecode = frames_to_timecode(marker_frame, formatted_frame_rate)
+            timeline_marker_list.append(str(marker_timecode))
+
+    # sort list and only keep unique values, necessary due FCPX assigning duplicate chapter-markers to asset-clips in .fcpxml files
+    timeline_marker_list = sorted(set(timeline_marker_list))
+    
+    print(*timeline_marker_list, sep='\n')
 
 def main():
     xml_file = input("Enter xml file path: ")
     parsed_xml = parse_xml(xml_file)
     timeline_info = get_timeline_info(parsed_xml)
-    clips_list = grab_clips(parsed_xml)
-    generate_output(clips_list, timeline_info)
+    clips_dict = grab_clips(parsed_xml)
+    generate_output(clips_dict, timeline_info)
 
 if __name__ == "__main__":
     main()
