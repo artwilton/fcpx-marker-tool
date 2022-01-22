5ce2b409aeec3a1df653f7d2d7cee5553c3d9bf7
Arthur <arthur.m.wilton@gmail.com>
2022-01-22 17:22:59 -0500

    named base Classes

---
+++ fcpx_marker_tool_v2.py
@@ -0,0 +1,42 @@
+"""
+This script takes FCPX .xml files as an input and prints chapter marker info
+"""
+
+import xml.etree.ElementTree as ET
+from timecode import Timecode
+
+class Timeline():
+    
+    def __init__(self):
+        return
+
+class Clip():
+
+    def __init__(self):
+        return
+
+class Marker():
+
+    def __init__(self):
+        return
+
+class XMLParser():
+
+    def __init__(self):
+        return
+
+class TimecodeHelpers():
+
+    def __init__(self):
+        return
+
+class Interface():
+
+    def __init__(self):
+        return
+
+def main():
+    print("main")
+
+if __name__ == "main":
+    main()
