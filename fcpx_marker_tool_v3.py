bf692a4074bae078e4b3d3edc29b4721edef4efd
Arthur <arthur.m.wilton@gmail.com>
2022-02-01 01:19:31 -0500

    created fcpx_marker_tool_v3.py as placeholder for next refactor

---
+++ fcpx_marker_tool_v3.py
@@ -0,0 +1,4 @@
+import xml.etree.ElementTree as ET
+from timecode import Timecode
+
+#This refactor will be built with the idea of making code more scalable, with future ability to import xml files from more than just FCPX
