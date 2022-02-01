6bdc03cfd98e473677a42a7e961ab91dea262766
Arthur <arthur.m.wilton@gmail.com>
2022-02-01 03:49:54 -0500

    added methods of displaying timecode

--- fcpx_marker_tool_v3.py
+++ fcpx_marker_tool_v3.py
@@ -10,9 +10,9 @@ class TimecodeInfo():
         self.frame_rate = frame_rate # can be rational string like '30000/1001', rational tuple (30000, 1001), int 30, or float 29.97
         # as notated in the Timecode module, frame_rate should be one of ['23.976', '23.98', '24', '25', '29.97', '30', '50', '59.94', '60', 'NUMERATOR/DENOMINATOR', ms'] where "ms" is equal to 1000 fps. 
         self.start = start # int representing start frame
-        self.duration = duration # int for amount of frames
+        self.duration = duration # int for total amount of frames
         self.non_drop_frame = non_drop_frame # Boolean, True for NDF and False for DF
-        self._timecode = Timecode(frame_rate, start, non_drop_frame)
+        self._timecode = Timecode(frame_rate, (start + 1), non_drop_frame)
     
     @property
     def frame_rate_number(self):
@@ -22,6 +22,16 @@ class TimecodeInfo():
     def frame_rate_tuple(self):
         # method that will return a tuple version regardless of how TimecodeInfo class is instantiated
         pass
+
+    @property
+    def standard_timecode(self):
+        return str(self._timecode)
+
+    @property
+    def fractional_timecode(self):
+        fractional_output = str(self._timecode.set_fractional(True))
+        self._timecode.set_fractional(False) #unset fractional timecode after grabbing output
+        return fractional_output
 
     @property
     def format(self):
