77e56fdceeba82b2c8fdef2762cae8a38711dd8f
Arthur <arthur.m.wilton@gmail.com>
2022-02-08 04:25:39 -0500

    updated main.py

--- fcpx_marker_tool/main.py
+++ fcpx_marker_tool/main.py
@@ -1,5 +1,9 @@
 def main():
-    print("running fcpx_marker_tool")
+    xml_parser = XMLParser.get_input("Enter file: ")
+    parser = xml_parser.create_parser()
+    parsed_xml = parser.parse_xml()
+    # tc = TimecodeInfo("12", (30000, 1001),10,10,True )
+    # print(tc.standard_timecode, tc.id, tc.frame_rate_number)
 
 if __name__ == "__main__":
     main()
