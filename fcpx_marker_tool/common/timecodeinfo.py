811a1373f32ec3d07fc70870f74ef7f6e02dc8c8
Arthur <arthur.m.wilton@gmail.com>
2022-02-10 03:11:26 -0500

    added offset to TimecodeInfo

--- fcpx_marker_tool/common/timecodeinfo.py
+++ fcpx_marker_tool/common/timecodeinfo.py
@@ -2,11 +2,12 @@ from timecode import Timecode
 
 class TimecodeInfo:
 
-    def __init__(self, id, frame_rate, start, duration, non_drop_frame=False):
+    def __init__(self, id, frame_rate, start, duration, offset=None, non_drop_frame=False):
         self.id = id # can be anything useful, example is "Format id" for FCPX
         self.frame_rate = frame_rate # can be rational string like '30000/1001', rational tuple (30000, 1001), int 30, or float 29.97
-        # as notated in the Timecode module, frame_rate should be one of ['23.976', '23.98', '24', '25', '29.97', '30', '50', '59.94', '60', 'NUMERATOR/DENOMINATOR', ms'] where "ms" is equal to 1000 fps. 
-        self.start = start # int representing start frame
+        # as notated in the Timecode module, frame_rate should be one of ['23.976', '23.98', '24', '25', '29.97', '30', '50', '59.94', '60', 'NUMERATOR/DENOMINATOR', ms'] where "ms" is equal to 1000 fps.
+        self.offset = offset # int representing start frame within a timeline, optional because usually only clips have this property
+        self.start = start # int representing start frame number
         self.duration = duration # int for total amount of frames
         self.non_drop_frame = non_drop_frame # Boolean, True for NDF and False for DF
         self._timecode = Timecode(frame_rate, frames=(start + 1), force_non_drop_frame=non_drop_frame)
