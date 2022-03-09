b91b5e0aa17dc7ead60ef289fb7fb647cf5146e7
Arthur <arthur.m.wilton@gmail.com>
2022-03-09 03:24:09 -0500

    changed non_drop_frame param to drop_frame, makes more sense to have NDF be default

--- fcpx_marker_tool/common/timecodeinfo.py
+++ fcpx_marker_tool/common/timecodeinfo.py
@@ -2,14 +2,14 @@ from timecode import Timecode
 
 class TimecodeInfo:
 
-    def __init__(self, frame_rate, start, duration, non_drop_frame=False, offset=None):
+    def __init__(self, frame_rate, start, duration, drop_frame=False, offset=None):
         self.frame_rate = frame_rate # can be rational string like '30000/1001', rational tuple (30000, 1001), int 30, or float 29.97
         # as notated in the Timecode module, frame_rate should be one of ['23.976', '23.98', '24', '25', '29.97', '30', '50', '59.94', '60', 'NUMERATOR/DENOMINATOR', ms'] where "ms" is equal to 1000 fps.
         self.start = start # int representing start frame number
         self.duration = duration # int for total amount of frames
-        self.non_drop_frame = non_drop_frame # Boolean, True for NDF and False for DF
+        self.drop_frame = drop_frame # Boolean, True for DF and False for NDF
         self.offset = offset # int representing start frame within a timeline, optional because usually only clips have this property
-        self._timecode = Timecode(frame_rate, frames=(start + 1), force_non_drop_frame=non_drop_frame)
+        self._timecode = Timecode(frame_rate, frames=(start + 1), force_non_drop_frame=drop_frame)
     
     @property
     def frame_rate_number(self):
@@ -33,7 +33,7 @@ class TimecodeInfo:
 
     @property
     def format(self):
-        if self.non_drop_frame:
-            return 'NDF'
-        else:
+        if self.drop_frame:
             return 'DF'
+        else:
+            return 'NDF'
