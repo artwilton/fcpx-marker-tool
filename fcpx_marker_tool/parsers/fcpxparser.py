460541ce91ee0847d38acf982570a9c548f46870
Arthur <arthur.m.wilton@gmail.com>
2022-03-19 01:41:59 -0400

    fixed issue where offset wasn't getting set as int

--- fcpx_marker_tool/parsers/fcpxparser.py
+++ fcpx_marker_tool/parsers/fcpxparser.py
@@ -1,3 +1,4 @@
+import copy
 from pathlib import Path
 from common import helpers
 from common.projectclasses import ProjectFile, Resource, Timeline, Clip, Container, Marker
@@ -100,6 +101,7 @@ class FCPXParser:
             start = helpers.get_number_of_frames(start, conformed_frame_rate_tuple)
             
         duration = helpers.get_number_of_frames(duration, frame_rate_tuple)
+        offset = helpers.get_number_of_frames(offset, frame_rate_tuple)
 
         timecode_info = TimecodeInfo(frame_rate_tuple, start, duration, non_drop_frame, offset)
 
@@ -115,34 +117,41 @@ class FCPXParser:
     
     def _filter_event_children(self, event_child):
         if event_child.tag.endswith('clip'):
-            event_child = self._handle_clip_creation(event_child, event_clip=True)
+            event_child = self._handle_clip_and_marker_creation(event_child, event_clip=True)
         elif event_child.tag == 'project':
             event_child = self._create_timeline(event_child)
 
         return event_child
 
-    def _handle_clip_creation(self, clip_element, timeline_frame_rate=None, timeline_ndf=None, event_clip=False):
-        name, start, duration, offset, type, resource_id, conformed_frame_rate_tuple = self._get_common_clip_info(clip_element)
+    def _handle_clip_and_marker_creation(self, clip_element, timeline_obj=None, event_clip=False):
+        conformed_frame_rate_tuple = self._conform_rate_check(clip_element)
+        clip_obj = self._create_clip(clip_element, timeline_obj, conformed_frame_rate_tuple, event_clip)
+        self._add_markers_to_clip(clip_element, clip_obj, conformed_frame_rate_tuple)
+
+        if event_clip is False:
+            self._add_markers_to_timeline(timeline_obj, clip_obj)
+
+        return clip_obj
+
+    def _create_clip(self, clip_element, timeline_obj, conformed_frame_rate_tuple, event_clip=False):
+        name, start, duration, offset, type, resource_id = self._get_common_clip_info(clip_element)
 
         if event_clip:
             frame_rate_tuple, non_drop_frame = self._get_event_clip_format_info(clip_element, resource_id)
         else:
-            frame_rate_tuple, non_drop_frame = timeline_frame_rate, timeline_ndf
+            frame_rate_tuple, non_drop_frame = timeline_obj.timecode_info.frame_rate, timeline_obj.timecode_info.non_drop_frame
 
         timecode_info = self._create_timecode_info(frame_rate_tuple, start, duration, non_drop_frame, offset, conformed_frame_rate_tuple)
 
         clip_obj = Clip(name, type, timecode_info, resource_id)
-        self._add_markers_to_clip(clip_element, clip_obj, conformed_frame_rate_tuple)
 
         return clip_obj
-
     def _get_common_clip_info(self, clip_element):
         name, start, duration, offset = helpers.get_attributes(clip_element, 'name', 'start', 'duration', 'offset')
         type = clip_element.tag
         resource_id = self._get_resource_id(clip_element)
-        conformed_frame_rate_tuple = self._conform_rate_check(clip_element)
 
-        return  name, start, duration, offset, type, resource_id, conformed_frame_rate_tuple
+        return  name, start, duration, offset, type, resource_id
 
     def _get_event_clip_format_info(self, clip_element, resource_id):
         format = clip_element.get('format')
@@ -230,8 +239,7 @@ class FCPXParser:
         # Grab metadata, create timeline instance
         name, timecode_info = self._get_timeline_info(timeline_element)
         timeline_obj = Timeline(name, timecode_info)
-        # create and add clip to timeline:
-        self._add_clips_to_timeline(timeline_element, timeline_obj)
+        self._add_clips_and_markers_to_timeline(timeline_element, timeline_obj)
 
         return timeline_obj
 
@@ -244,23 +252,20 @@ class FCPXParser:
 
         return name, timecode_info
 
-    def _add_clips_to_timeline(self, timeline_element, timeline_obj):
-        timeline_clip_list = []
+    def _add_clips_and_markers_to_timeline(self, timeline_element, timeline_obj):
 
-        for clip in timeline_element.iterfind('./sequence/spine/'):
-            primary_clip = self._create_timeline_clip(clip, timeline_obj)
-            connected_clips = [self._create_timeline_clip(clip, timeline_obj) for clip in clip.iterfind('./[@lane]')]
-            timeline_clip_list.append(primary_clip)
-            timeline_clip_list.extend(connected_clips)
+        primary_clips = timeline_element.iterfind('./sequence/spine/')
+        primary_clips_formatted = [self._check_for_audition(clip) for clip in primary_clips]
 
-        timeline_obj.clips = timeline_clip_list
+        for primary_clip in primary_clips_formatted:
+            primary_clip_obj = self._handle_clip_and_marker_creation(primary_clip, timeline_obj)
+            timeline_obj.add_clip(primary_clip_obj)
 
-    def _create_timeline_clip(self, clip_element, timeline_obj):
-        frame_rate, non_drop_frame = timeline_obj.timecode_info.frame_rate, timeline_obj.timecode_info.non_drop_frame
-        formatted_clip = self._check_for_audition(clip_element)
-        clip_obj = self._handle_clip_creation(formatted_clip, frame_rate, non_drop_frame)
-        
-        return clip_obj
+            for connected_clip in primary_clip.iterfind('*[@lane]'):
+                connected_clip_formatted = self._check_for_audition(connected_clip)
+                print("FOUND CONNECTED CLIP")
+                connected_clip_obj = self._handle_clip_and_marker_creation(connected_clip_formatted, timeline_obj)
+                timeline_obj.add_clip(connected_clip_obj)
 
     def _check_for_audition(self, clip_element):
         if clip_element.tag == 'audition':
@@ -294,6 +299,19 @@ class FCPXParser:
         timecode_info = self._create_timecode_info(frame_rate_tuple, start, duration, non_drop_frame, offset, conformed_frame_rate_tuple)
 
         return Marker(name, marker_type, timecode_info, completed)
+
+    def _add_markers_to_timeline(self, timeline_obj, clip_obj):
+        for marker in clip_obj.markers:
+            marker_start = marker.timecode_info.start
+            clip_start, clip_offset, clip_duration = clip_obj.timecode_info.start, clip_obj.timecode_info.offset, clip_obj.timecode_info.duration
+            clip_end = clip_offset + clip_duration
+            marker_timeline_start = (marker_start - clip_start) + clip_offset
+
+            if (marker_timeline_start >= clip_offset) and (marker_timeline_start <= clip_end):
+                timeline_marker = copy.deepcopy(marker)
+                timeline_marker.timecode_info.start = marker_timeline_start
+                timeline_obj.add_marker(timeline_marker)
+                print(timeline_marker.timecode_info.start, marker_timeline_start)
 
     def parse_xml(self):
         self._create_resources(self.project_file)
